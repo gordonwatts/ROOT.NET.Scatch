6a0c6285615c87ada9c7bd693d131e82b53c9b63
Gordon Watts <gordon@gordonandpaula.net>
2014-09-21 03:37:51 +0200

    Added a nuget consumer, got it working
    Figured out who to get this to work nicely with a pacakge dependency!

---
+++ CPPBaseLibraryNugetConsumer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPPBaseLibraryNugetConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new CPPBaseLibraryWrapper.ClassWrapper();
+            Console.WriteLine(c.GetValue());
+        }
+    }
+}
